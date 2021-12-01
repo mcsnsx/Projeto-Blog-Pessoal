12ea6aa74918dd3146cff262a3a6261d8b5938f9
mcsnsx <mcs_nasc@hotmail.com>
2021-12-01 16:47:53 -0300

    Blog Pessoal 01.12

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
+import CadastroUsuario from './paginas/CadastroUsuario/CadastroUsuario';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Longin';
 import './App.css';
 
 
@@ -9,11 +12,32 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
+        <Switch>
+          <div style={{minHeight: "100vh"}}>
+
+          <Route exact path='/'>
+              <Login />
+
+            </Route>
+
+          <Route path='/login'>
+              <Login />
+
+            </Route>
+            <Route path='/home'>
+              <Home />
+
+              </Route>
+            <Route path='/cadastrousuario'>
+              <CadastroUsuario />
+
+            </Route>
+          </div>
+        </Switch>
       <Footer />
-    </>
+    </Router>
   );
 }
 
