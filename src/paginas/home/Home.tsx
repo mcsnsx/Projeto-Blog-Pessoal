252ecbc90895cdd997198d86a7faa48abf96a146
mcsnsx <mcs_nasc@hotmail.com>
2022-03-16 12:50:54 -0300

    Ajuste de posicionamento Home

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -40,9 +40,9 @@ function Home() {
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
                 <Grid alignItems="center" item xs={6}>
-                    <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    <Box paddingX={10} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Bem vindo(a)!</Typography>
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h6" align="center" className='subtitulo'>Seja livre para expressar seus pensamentos e opiniões!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}> 
@@ -54,7 +54,7 @@ function Home() {
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
-                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                    <img src="https://imgur.com/CvGU5uL.jpg" alt="" width="500px" height="300px" />
                 </Grid>
                 <Grid xs={12} className='postagens'>
                 <TabPostagem />
